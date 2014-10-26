a9716c7ca2ab183592d36d243a3e47444a298ca6
Debarghya Das <dd367@cornell.edu>
2014-10-26 19:55:40 -0400

    more correct specgram progress

--- parse_mp3.py
+++ parse_mp3.py
@@ -2,9 +2,15 @@ from pydub import AudioSegment
 import eyed3
 import os.path
 import numpy as np
+import time
+
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib.cm as cm
+# from matplotlib.colors import LogNorm
 
 from IPython.core.debugger import Tracer
+
 
 
 def parse_mp3(path):
@@ -18,15 +24,31 @@ def parse_mp3(path):
   print('\n\n\n')
 
   print('Decoding audio (this may take a while) ... \n')
-  audio = AudioSegment.from_mp3('data/duaa.mp3')
-  print('Done decoding.\n')
+  decode_start_time = time.time()
+  audio = AudioSegment.from_file(path)
+  print('Done decoding in {0} seconds to decode {1:.2f} seconds of audio.\n'.format(time.time() - decode_start_time, len(audio)))
 
-  data = np.fromstring(audio._data, np.int16)
+  # Get last 30 seconds
+  audio = audio[-40000:-10000]
 
-  channels = []
-  for channel in xrange(audio.channels):
-    channels.append(data[channel::audio.channels])
+  # np.int32 or np.int16?
+  data = np.fromstring(audio._data, np.int32)
+
+  # What is the correct way to separate data into channels?
+  # channels = []
+  # for channel in xrange(audio.channels):
+  #   channels.append(data[channel::audio.channels])
+
+  Pxx, freqs, bins = get_spectrogram(data, plot=True)
   Tracer()()
+
+  # Maybe do a 3D visual?
+  # from mpl_toolkits.mplot3d import Axes3D
+  # fig = plt.figure()
+  # ax = fig.gca(projection='3d')
+  # B, F = meshgrid(bins, freqs)
+  # ax.plot_surface(B, F, Pxx, cstride=10, rstride=10, cmap=cm.coolwarm)
+  # show()
 
 def print_song_metadata(id3tags):
   print('---------- SONG DETAILS ----------')
@@ -41,3 +63,28 @@ def print_audio_metadata(id3tags):
   print('Sample Frequency: {0}'.format(id3tags.info.sample_freq))
   print('Size (in bytes):  {0}'.format(id3tags.info.size_bytes))
   print('Time (in s):      {0}'.format(id3tags.info.time_secs))
+
+DEFAULT_WINDOW_SIZE = 4096
+DEFAULT_OVERLAP_RATIO = 0.5
+DEFAULT_SAMPLE_FREQUENCY = 44100
+
+def get_spectrogram(data,
+    nfft = DEFAULT_WINDOW_SIZE,
+    overlap = DEFAULT_OVERLAP_RATIO,
+    fs = DEFAULT_SAMPLE_FREQUENCY,
+    plot = False):
+  # matplotlib.pyplot and matplotlib have different specgram implementations -
+  # pyplot plots, and mlab merely calculates.
+  if plot:
+    ax = plt.subplot(111)
+    Pxx, freqs, bins, im = ax.specgram(data, NFFT = nfft, Fs = fs, noverlap = (int(nfft*overlap)))
+    plt.xlabel('Time (in seconds)')
+    plt.ylabel('Frequency (in Hz)')
+    plt.xlim(0, len(data)/fs)
+    plt.ylim(0, fs//2) # Nyquist sampling
+    plt.show()
+    return Pxx, freqs, bins
+  Pxx, freqs, bins = mlab.specgram(data, NFFT = nfft, Fs = fs, noverlap = (int(nfft*overlap)))
+  return Pxx, freqs, bins
+
+
