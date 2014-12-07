Fixed tab spacing and logistic regression over/underflow problems

eval.py
@@ -15,140 +15,140 @@ import sys
 import math
 
 def split_playsets(data, T=0.5):
-    """
-    Split playsets into training and test sets
+  """
+  Split playsets into training and test sets
 
-    Each playset gets split in T (as opposed to splitting the 
-    list of playsets in T and keeping individual playsets together)
+  Each playset gets split in T (as opposed to splitting the 
+  list of playsets in T and keeping individual playsets together)
 
-    The test set will not contain any songs from the training set
+  The test set will not contain any songs from the training set
 
-    :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
-    A list of playsets containing song IDs, and a dictionary
-    that maps song IDs to AudioFeatureSet objects
-    :param int T: Fraction of instances in training set
-    (1 - fraction of instances in test set)
-    :rtype tuple(train,test)
-    :return A tuple of training and test data sets
-    """
+  :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
+  A list of playsets containing song IDs, and a dictionary
+  that maps song IDs to AudioFeatureSet objects
+  :param int T: Fraction of instances in training set
+  (1 - fraction of instances in test set)
+  :rtype tuple(train,test)
+  :return A tuple of training and test data sets
+  """
 
-    playsets, afshash = data
-    def split(x):
-        shuffle(x)
-        s = int(T*len(x))
-        return (x[:s],x[s:])
-    pre = [split(list(ps)) for ps in playsets]
-    train = [x[0] for x in pre]
-    test = [x[1] for x in pre]
-    songs = lambda t: {s for ps in t for s in ps}
-    pare = lambda t, h: {k:v for k, v in h.iteritems() if k in songs(t)}
-    return ((train, pare(train, afshash)), (test, pare(test, afshash)))
+  playsets, afshash = data
+  def split(x):
+    shuffle(x)
+    s = int(T*len(x))
+    return (x[:s],x[s:])
+  pre = [split(list(ps)) for ps in playsets]
+  train = [x[0] for x in pre]
+  test = [x[1] for x in pre]
+  songs = lambda t: {s for ps in t for s in ps}
+  pare = lambda t, h: {k:v for k, v in h.iteritems() if k in songs(t)}
+  return ((train, pare(train, afshash)), (test, pare(test, afshash)))
 
 def song_pairs_mse(model, data):
-    """
-    Compute mean-squared error of predicted conditional probabilities
-    of song co-occurrence
+  """
+  Compute mean-squared error of predicted conditional probabilities
+  of song co-occurrence
 
-    :param SongPairModel model: A trained song similarity regression model
-    :param list[tuple(AudioFeatureSet,AudioFeatureSet,float)] data: 
-    A list of (song A, song B, \hat{Pr}(A|B))
-    :rtype float
-    :return Mean-squared error
-    """
+  :param SongPairModel model: A trained song similarity regression model
+  :param list[tuple(AudioFeatureSet,AudioFeatureSet,float)] data: 
+  A list of (song A, song B, \hat{Pr}(A|B))
+  :rtype float
+  :return Mean-squared error
+  """
 
-    pred, act = [], []
-    for d in data:
-        x, y, p = d
-        pred.append(math.exp(model.log_likelihood(x, y)))
-        act.append(p)
+  pred, act = [], []
+  for d in data:
+    x, y, p = d
+    pred.append(math.exp(model.log_likelihood(x, y)))
+    act.append(p)
 
-    # BEGIN DEBUG
-    """
-    plt.scatter(pred, act)
-    plt.show()
-    plt.hist(act, color='blue')
-    plt.hist(pred, color='red')
-    plt.show()
-    """
-    # END DEBUG
+  # BEGIN DEBUG
+  """
+  plt.scatter(pred, act)
+  plt.show()
+  plt.hist(act, color='blue')
+  plt.hist(pred, color='red')
+  plt.show()
+  """
+  # END DEBUG
 
-    mse = np.linalg.norm(np.asarray(pred) - np.asarray(act)) / len(pred)
+  mse = np.linalg.norm(np.asarray(pred) - np.asarray(act)) / len(pred)
 
-    return mse
+  return mse
 
 def playsets_avg_ll(model, data):
-    """
-    Compute average log-likelihood of playsets
+  """
+  Compute average log-likelihood of playsets
 
-    :param PlaysetModel model: A trained playset model
-    :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
-    A list of playsets containing song IDs, and a dictionary
-    that maps song IDs to AudioFeatureSet objects
-    :rtype float
-    :return Average log-likelihood
-    """
+  :param PlaysetModel model: A trained playset model
+  :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
+  A list of playsets containing song IDs, and a dictionary
+  that maps song IDs to AudioFeatureSet objects
+  :rtype float
+  :return Average log-likelihood
+  """
 
-    playsets, afshash = data
-    avg_ll = sum(model.avg_log_likelihood((ps, afshash)) for ps in playsets) / len(playsets)
-    return avg_ll
+  playsets, afshash = data
+  avg_ll = sum(model.avg_log_likelihood((ps, afshash)) for ps in playsets) / len(playsets)
+  return avg_ll
 
 def evaluate_models(data, K=3):
-    """
-    Compute cross-validated average log-likelihood
-    of held out playsets using models.PlaysetModel
+  """
+  Compute cross-validated average log-likelihood
+  of held out playsets using models.PlaysetModel
 
-    Compute cross-validated mean-squared error
-    of held out song pairs using models.SongPairModel
+  Compute cross-validated mean-squared error
+  of held out song pairs using models.SongPairModel
 
-    :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
-    A list of playsets containing song IDs, and a dictionary
-    that maps song IDs to AudioFeatureSet objects
-    :param int K: K-fold cross-validation
-    :rtype tuple(tuple(float,float),tuple(float,float))
-    :return Average log-likelihood and mean-squared error
-    of model vs. benchmark
-    """
-    
-    model = PlaysetModel()
-    bench_model = PlaysetModel(benchmark=True)
-    bench_song_pair_model = BenchmarkSongPairModel()
-    avg_ll = 0
-    bench_avg_ll = 0
-    mse = 0
-    bench_mse = 0
-    for _ in xrange(K):
-        train, test = split_playsets(data)
-        model.train(train)
-        bench_model.train(train)
-        avg_ll += playsets_avg_ll(model, test)
-        bench_avg_ll += playsets_avg_ll(bench_model, test)
+  :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
+  A list of playsets containing song IDs, and a dictionary
+  that maps song IDs to AudioFeatureSet objects
+  :param int K: K-fold cross-validation
+  :rtype tuple(tuple(float,float),tuple(float,float))
+  :return Average log-likelihood and mean-squared error
+  of model vs. benchmark
+  """
+  
+  model = PlaysetModel()
+  bench_model = PlaysetModel(benchmark=True)
+  bench_song_pair_model = BenchmarkSongPairModel()
+  avg_ll = 0
+  bench_avg_ll = 0
+  mse = 0
+  bench_mse = 0
+  for _ in xrange(K):
+    train, test = split_playsets(data)
+    model.train(train)
+    bench_model.train(train)
+    avg_ll += playsets_avg_ll(model, test)
+    bench_avg_ll += playsets_avg_ll(bench_model, test)
 
-        test_song_pairs = get_song_pairs(test)
-        mse += song_pairs_mse(model.song_pair_model, test_song_pairs)
-        bench_mse += song_pairs_mse(bench_song_pair_model, test_song_pairs)
+    test_song_pairs = get_song_pairs(test)
+    mse += song_pairs_mse(model.song_pair_model, test_song_pairs)
+    bench_mse += song_pairs_mse(bench_song_pair_model, test_song_pairs)
 
-    return ((avg_ll/K), (bench_avg_ll/K), (mse/K), (bench_mse/K))
+  return ((avg_ll/K), (bench_avg_ll/K), (mse/K), (bench_mse/K))
 
 def main():
-    #data_path = sys.argv[1]
-    #print "Munging GTZAN..."
-    #data = munge_gtzan(data_path)
-    #print "Done."
-    data = generate_synthetic_playsets()
+  #data_path = sys.argv[1]
+  #print "Munging GTZAN..."
+  #data = munge_gtzan(data_path)
+  #print "Done."
+  data = generate_synthetic_playsets()
 
-    # BEGIN DEBUG
-    """
-    cp = [x[2] for x in get_song_pairs(data)]
-    plt.hist(cp)
-    plt.show()
-    """
-    # END DEBUG
+  # BEGIN DEBUG
+  """
+  cp = [x[2] for x in get_song_pairs(data)]
+  plt.hist(cp)
+  plt.show()
+  """
+  # END DEBUG
 
-    avg_ll, bench_avg_ll, mse, bench_mse = evaluate_models(data)
+  avg_ll, bench_avg_ll, mse, bench_mse = evaluate_models(data)
 
-    print "Playset Model\n-------------\nAverage log-likelihood: %f\nMean-squared error %f" % (avg_ll, mse)
-    print ""
-    print "Benchmark Model\n-------------\nAverage log-likelihood: %f\nMean-squared error %f" % (bench_avg_ll, bench_mse)
+  print "Playset Model\n-------------\nAverage log-likelihood: %f\nMean-squared error %f" % (avg_ll, mse)
+  print ""
+  print "Benchmark Model\n-------------\nAverage log-likelihood: %f\nMean-squared error %f" % (bench_avg_ll, bench_mse)
 
 if __name__ == '__main__':
-    main()
+  main()

models.py
@@ -23,196 +23,196 @@ import uuid
 import os
 
 class AudioFeatureSet(object):
+  """
+  Class for mapping mel-spectrograms to clean,
+  standardized audio feature sets
+  """
+
+  def __init__(self, x):
     """
-    Class for mapping mel-spectrograms to clean,
-    standardized audio feature sets
+    Initialize audio feature set object
+
+    :param AudioBite|np.ndarray x: 
     """
+  
+    if isinstance(x, AudioBite):
+      self.vec = x.mel_specgram.flatten() # DEBUG
+    elif isinstance(x, np.ndarray):
+      self.vec = x
+    else:
+      raise Exception("Type error!")
 
-    def __init__(self, x):
-        """
-        Initialize audio feature set object
+    #self.fid = str(uuid.uuid4())
+    #pickle.dump(self.vec, open(os.path.join('afs', self.fid + '.pkl'), 'wb'))
+    #self.vec = None
 
-        :param AudioBite|np.ndarray x: 
-        """
-    
-        if isinstance(x, AudioBite):
-            self.vec = x.mel_specgram.flatten() # DEBUG
-        elif isinstance(x, np.ndarray):
-            self.vec = x
-        else:
-            raise Exception("Type error!")
-
-        #self.fid = str(uuid.uuid4())
-        #pickle.dump(self.vec, open(os.path.join('afs', self.fid + '.pkl'), 'wb'))
-        #self.vec = None
-
-    def get(self):
-        return self.vec
-        #return pickle.load(open(os.path.join('afs', self.fid + '.pkl'), 'rb'))
+  def get(self):
+    return self.vec
+    #return pickle.load(open(os.path.join('afs', self.fid + '.pkl'), 'rb'))
 
 class SongPairModel(object):
+  """
+  Class for regression model that maps two audio feature
+  sets to a probability of co-occurrence in a playset
+
+  #Uses Kernelized Support Vector Regression
+  Uses Logistic Regression with L2 regularization
+  """
+
+  def __init__(self):
+    self.model = None
+
+  def join_feature_sets(self, x, y):
     """
-    Class for regression model that maps two audio feature
-    sets to a probability of co-occurrence in a playset
+    Concatenate two song audio feature sets
 
-    #Uses Kernelized Support Vector Regression
-    Uses Logistic Regression with L2 regularization
-    """
-
-    def __init__(self):
-        self.model = None
-
-    def join_feature_sets(self, x, y):
-        """
-        Concatenate two song audio feature sets
-
-        :param AudioFeatureSet x: An audio feature set of a song
-        :param AudioFeatureSet y: An audio feature set of a song
-        :rtype np.ndarray
-        :return Feature set of x :: feature set of y
-        """
-
-        return np.concatenate((x.get(), y.get()))
-
-    def train(self, data):
-        """
-        Train regression model on a training data set
-
-        :param list[tuple(AudioFeatureSet,AudioFeatureSet,float)] data: 
-        A list of (song A, song B, \hat{Pr}(A|B))
-        """
-        
-        print "Preparing training set..."
-        X = [self.join_feature_sets(e[0], e[1]) for e in data]
-        Y = [e[2] for e in data]
-        print "Done."
-
-        print "Training model on data set with %d instances and %d features..." % (len(X), len(X[0]))
-        start_time = time.time()
-        #self.model = KNeighborsRegressor(n_neighbors=3)
-        self.model = LogisticRegression()
-        #self.model = SVR(kernel='rbf', C=1.0)
-        #self.model = SVC(kernel='rbf', C=1.0, probability=True)
-        #self.model = MLP()
-        self.model.fit(X, Y)
-        print "Done in %f seconds." % (time.time() - start_time)
-
-    def log_likelihood(self, x, y):
-        """
-        Compute log likelihood of co-occurrence Pr(X|Y)
-
-        :param AudioFeatureSet x: An audio feature set of a song
-        :param AudioFeatureSet y: An audio feature set of a song
-        :rtype float
-        :return A real value between 0 and 1
-        """
-
-        f = self.join_feature_sets(x, y)
-        return self.model.predict_log_proba(f)
-        #return self.model.predict(f)
-
-class BenchmarkSongPairModel(object):
-    def __init__(self):
-        pass
-
-    def train(self, data):
-        pass
-
-    def log_likelihood(self, x, y):
-        r = random()
-        return log(r)
-
-class PlaysetModel(object):
-    """
-    Class for generative model of playsets
+    :param AudioFeatureSet x: An audio feature set of a song
+    :param AudioFeatureSet y: An audio feature set of a song
+    :rtype np.ndarray
+    :return Feature set of x :: feature set of y
     """
 
-    def __init__(self, benchmark=False):
-        self.song_pair_model = None
-        self.benchmark = benchmark 
+    return np.concatenate((x.get(), y.get()))
 
-    def train(self, data):
-        """
-        Estimate generative model using a training data set
-
-        :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
-        A list of playsets containing song IDs, and a dictionary
-        that maps song IDs to AudioFeatureSet objects
-        """
-
-        pairs = get_song_pairs(data)
-
-        self.song_pair_model = BenchmarkSongPairModel() if self.benchmark else SongPairModel()
-        self.song_pair_model.train(pairs)
-
-    def log_cond_prob(self, x, y):
-        """
-        Compute the conditional probability of observing songs
-        x={x_1,x_2,...,x_n} in a playset given that song y is 
-        already in the playset 
-
-        Assumes conditional independence of all x_i given y,
-        which leads to the following factorization:
-        P(x|y) = \prod_{i=1}^n P(x_i|y)
-
-        :param tuple(set(str), dict[str]=AudioFeatureSet) x: A playset
-        :param tuple(str, AudioFeatureSet) y: A song
-        :rtype float
-        :return Log of the conditional probability P(x|y)
-        """
-
-        ps, afshash = x
-        s, afs = y
-
-        log_cond = sum(self.song_pair_model.log_likelihood(afshash[xi], afs) for xi in ps)
-
-        return log_cond
-
-    def avg_log_likelihood(self, x):
-        """
-        Compute the average log-likelihood of observing a playset
-        under the current model
-
-        :param tuple(set(str),dict[str]=AudioFeatureSet) x:
-        A playset containing song IDs, and a dictionary
-        that maps song IDs to AudioFeatureSet objects 
-        :rtype float
-        :return Log-likelihood of playset
-        """
-
-        ps, afshash = x
-        ps = list(ps)
-        avg_ll = sum(
-            self.song_pair_model.log_likelihood(
-                afshash[a], afshash[b]) for i, a in enumerate(
-                ps) for b in ps[(i+1):]) / (len(ps)*(len(ps)-1)/2)
-
-        return avg_ll
-
-def get_song_pairs(data):
+  def train(self, data):
     """
-    Generate song pairs and estimated conditional 
-    probabilities of co-occurrence from a list
-    of playsets
+    Train regression model on a training data set
 
-    :param list[set(str)] data: A list of playsets
-    :rtype list[tuple(AudioFeatureSet,AudioFeatureSet,float)] 
-    :return A list of (song A, song B, \hat{Pr}(A|B))
+    :param list[tuple(AudioFeatureSet,AudioFeatureSet,float)] data: 
+    A list of (song A, song B, \hat{Pr}(A|B))
     """
     
-    playsets, afshash = data
-    songs = {x for ps in playsets for x in ps}
-    P = {x:0 for x in songs}
-    CP = {(x,y):0 for x in songs for y in songs if x!=y}
+    print "Preparing training set..."
+    X = [self.join_feature_sets(e[0], e[1]) for e in data]
+    Y = [e[2] for e in data]
+    print "Done."
 
-    for ps in playsets:
-        ps = list(ps)
-        for i, x in enumerate(ps):
-            P[x] += 1
-            for y in ps[(i+1):]:
-                CP[(x,y)] += 1
+    print "Training model on data set with %d instances and %d features..." % (len(X), len(X[0]))
+    start_time = time.time()
+    #self.model = KNeighborsRegressor(n_neighbors=3)
+    self.model = LogisticRegression()
+    #self.model = SVR(kernel='rbf', C=1.0)
+    #self.model = SVC(kernel='rbf', C=1.0, probability=True)
+    #self.model = MLP()
+    self.model.fit(X, Y)
+    print "Done in %f seconds." % (time.time() - start_time)
 
-    num_songs = len(songs)
-    CP = {k:(v/P[k[1]]) for k, v in CP.iteritems()}
+  def log_likelihood(self, x, y):
+    """
+    Compute log likelihood of co-occurrence Pr(X|Y)
 
-    return [(afshash[x],afshash[y],CP[(x,y)]) for x in songs for y in songs if x!=y]
+    :param AudioFeatureSet x: An audio feature set of a song
+    :param AudioFeatureSet y: An audio feature set of a song
+    :rtype float
+    :return A real value between 0 and 1
+    """
+
+    f = self.join_feature_sets(x, y)
+    return self.model.predict_log_proba(f)
+    #return self.model.predict(f)
+
+class BenchmarkSongPairModel(object):
+  def __init__(self):
+    pass
+
+  def train(self, data):
+    pass
+
+  def log_likelihood(self, x, y):
+    r = random()
+    return log(r)
+
+class PlaysetModel(object):
+  """
+  Class for generative model of playsets
+  """
+
+  def __init__(self, benchmark=False):
+    self.song_pair_model = None
+    self.benchmark = benchmark 
+
+  def train(self, data):
+    """
+    Estimate generative model using a training data set
+
+    :param tuple(list[set(str)],dict[str]=AudioFeatureSet) data: 
+    A list of playsets containing song IDs, and a dictionary
+    that maps song IDs to AudioFeatureSet objects
+    """
+
+    pairs = get_song_pairs(data)
+
+    self.song_pair_model = BenchmarkSongPairModel() if self.benchmark else SongPairModel()
+    self.song_pair_model.train(pairs)
+
+  def log_cond_prob(self, x, y):
+    """
+    Compute the conditional probability of observing songs
+    x={x_1,x_2,...,x_n} in a playset given that song y is 
+    already in the playset 
+
+    Assumes conditional independence of all x_i given y,
+    which leads to the following factorization:
+    P(x|y) = \prod_{i=1}^n P(x_i|y)
+
+    :param tuple(set(str), dict[str]=AudioFeatureSet) x: A playset
+    :param tuple(str, AudioFeatureSet) y: A song
+    :rtype float
+    :return Log of the conditional probability P(x|y)
+    """
+
+    ps, afshash = x
+    s, afs = y
+
+    log_cond = sum(self.song_pair_model.log_likelihood(afshash[xi], afs) for xi in ps)
+
+    return log_cond
+
+  def avg_log_likelihood(self, x):
+    """
+    Compute the average log-likelihood of observing a playset
+    under the current model
+
+    :param tuple(set(str),dict[str]=AudioFeatureSet) x:
+    A playset containing song IDs, and a dictionary
+    that maps song IDs to AudioFeatureSet objects 
+    :rtype float
+    :return Log-likelihood of playset
+    """
+
+    ps, afshash = x
+    ps = list(ps)
+    avg_ll = sum(
+      self.song_pair_model.log_likelihood(
+        afshash[a], afshash[b]) for i, a in enumerate(
+        ps) for b in ps[(i+1):]) / (len(ps)*(len(ps)-1)/2)
+
+    return avg_ll
+
+def get_song_pairs(data):
+  """
+  Generate song pairs and estimated conditional 
+  probabilities of co-occurrence from a list
+  of playsets
+
+  :param list[set(str)] data: A list of playsets
+  :rtype list[tuple(AudioFeatureSet,AudioFeatureSet,float)] 
+  :return A list of (song A, song B, \hat{Pr}(A|B))
+  """
+  
+  playsets, afshash = data
+  songs = {x for ps in playsets for x in ps}
+  P = {x:0 for x in songs}
+  CP = {(x,y):0 for x in songs for y in songs if x!=y}
+
+  for ps in playsets:
+    ps = list(ps)
+    for i, x in enumerate(ps):
+      P[x] += 1
+      for y in ps[(i+1):]:
+        CP[(x,y)] += 1
+
+  num_songs = len(songs)
+  CP = {k:(v/P[k[1]]) for k, v in CP.iteritems()}
+
+  return [(afshash[x],afshash[y],CP[(x,y)]) for x in songs for y in songs if x!=y]

playsets.py
@@ -15,68 +15,68 @@ import sys
 import numpy as np
 
 def grow_playset(model, x, pool):
-    """
-    Sample a new song to add to a playset
+  """
+  Sample a new song to add to a playset
 
-    :param PlaysetModel model: A trained generative model
-    of playsets
-    :param tuple(set(str), dict[str]=AudioFeatureSet) x: A playset
-    :param dict[str]=AudioFeatureSet pool: A pool of possible songs
-    to add to the playset
-    :rtype tuple(set(str), dict[str]=AudioFeatureSet)
-    :return The playset x, with one new song
-    """
+  :param PlaysetModel model: A trained generative model
+  of playsets
+  :param tuple(set(str), dict[str]=AudioFeatureSet) x: A playset
+  :param dict[str]=AudioFeatureSet pool: A pool of possible songs
+  to add to the playset
+  :rtype tuple(set(str), dict[str]=AudioFeatureSet)
+  :return The playset x, with one new song
+  """
 
-    ps, afshash = x
-    p = [exp(model.log_cond_prob(x, (y, afs))) for y, afs in pool.iteritems()]
-    z = sum(p)
-    p = [e/z for e in p]
-    y = np.random.choice(pool.keys(), p=p)
-    afshash[y] = pool[y]
-    del pool[y]
-    return (ps|{y}, afshash), pool
+  ps, afshash = x
+  p = [exp(model.log_cond_prob(x, (y, afs))) for y, afs in pool.iteritems()]
+  z = sum(p)
+  p = [e/z for e in p]
+  y = np.random.choice(pool.keys(), p=p)
+  afshash[y] = pool[y]
+  del pool[y]
+  return (ps|{y}, afshash), pool
 
 def make_playset(model, seed, pool, length=10):
-    """
-    Sample a playset
+  """
+  Sample a playset
 
-    :param PlaysetModel model: A trained generative model
-    of playsets
-    :param tuple(set(str), dict[str]=AudioFeatureSet) seed: A playset seed 
-    (should be non-empty), usually provided
-    by the user
-    :param dict[str]=AudioFeatureSet pool: A pool of possible songs
-    to include in the playset
-    :param int length: Desired playset length
-    :rtype set(str)
-    :return A playset sampled using a trained
-    generative model
-    """
+  :param PlaysetModel model: A trained generative model
+  of playsets
+  :param tuple(set(str), dict[str]=AudioFeatureSet) seed: A playset seed 
+  (should be non-empty), usually provided
+  by the user
+  :param dict[str]=AudioFeatureSet pool: A pool of possible songs
+  to include in the playset
+  :param int length: Desired playset length
+  :rtype set(str)
+  :return A playset sampled using a trained
+  generative model
+  """
 
-    ps, afshash = seed
-    assert length <= len(pool), "Playset larger than song pool!"
-    assert length >= len(ps), "Playset is already larger than desired length!"
+  ps, afshash = seed
+  assert length <= len(pool), "Playset larger than song pool!"
+  assert length >= len(ps), "Playset is already larger than desired length!"
 
-    for _ in xrange(length-len(ps)):
-        seed, pool = grow_playset(model, seed, pool)
+  for _ in xrange(length-len(ps)):
+    seed, pool = grow_playset(model, seed, pool)
 
-    return seed
+  return seed
 
 def main():
-    data_path = sys.argv[1]
-    data = munge_gtzan(data_path)
-    #data = generate_synthetic_playsets()
-    pool = data[1]
-    seed_song = np.random.choice(pool.keys())
-    seed_afshash = {seed_song:pool[seed_song]}
-    seed = (set([seed_song]), seed_afshash)
-    model = PlaysetModel()#benchmark=True)
-    model.train(data)
-    ps = make_playset(model, seed, pool)
+  data_path = sys.argv[1]
+  data = munge_gtzan(data_path)
+  #data = generate_synthetic_playsets()
+  pool = data[1]
+  seed_song = np.random.choice(pool.keys())
+  seed_afshash = {seed_song:pool[seed_song]}
+  seed = (set([seed_song]), seed_afshash)
+  model = PlaysetModel()#benchmark=True)
+  model.train(data)
+  ps = make_playset(model, seed, pool)
 
-    print "Seed: %s" % (seed_song)
-    print "Generated playset:"
-    print '\n'.join(ps[0])
+  print "Seed: %s" % (seed_song)
+  print "Generated playset:"
+  print '\n'.join(ps[0])
 
 if __name__ == '__main__':
-    main()
+  main()

reg.py
@@ -5,61 +5,69 @@ Module for logistic regression with Theano
 """
 
 from __future__ import division
-from math import log, exp
+from math import log, exp, tanh
 
 import theano
 import theano.tensor as T
 
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 
 import numpy as np
 
 class LogisticRegression(object):
 
-    def __init__(self):
-        self.w = None
-        self.b = None
+  def __init__(self):
+    self.w = None
+    self.b = None
 
-    def fit(self, X, Y, lrate=0.001, training_steps=100):#, batch_size=1):
-        """
-        Batch gradient descent for maximum-likelihood estimation
-        """
-        #num_instances = len(X)
-        num_features = len(X[0])
+  def fit(self, X, Y, lrate=0.001, training_steps=100, coeff_reg=0.1):#, batch_size=1):
+    """
+    Batch gradient descent for maximum-likelihood estimation
+    """
+    #num_instances = len(X)
+    num_features = len(X[0])
 
-        x = T.matrix("x")
-        y = T.vector("y")
-        w = theano.shared(np.random.random((num_features,)), name="w")
-        b = theano.shared(0., name="b")
+    x = T.matrix("x")
+    y = T.vector("y")
+    w = theano.shared(np.random.random((num_features,)), name="w")
+    b = theano.shared(0., name="b")
 
-        p = 1 / (1 + T.exp(-(T.dot(x, w) + b)))
-        cost = ((p - y) ** 2).sum() + (w ** 2).sum()
-        gw, gb = T.grad(cost, [w, b])
+    #p = 1 / (1 + T.exp(-(T.dot(x, w) + b)))
+    p = (1 + T.tanh(-(T.dot(x, w) + b))) / 2
+    cost = ((p - y) ** 2).sum() + coeff_reg * (w ** 2).sum()
+    gw, gb = T.grad(cost, [w, b])
 
-        train = theano.function(
-            inputs=[x,y],
-            outputs=[cost],
-            updates=((w, w - lrate * gw), (b, b - lrate * gb)))
+    train = theano.function(
+      inputs=[x,y],
+      outputs=[cost],
+      updates=((w, w - lrate * gw), (b, b - lrate * gb)))
 
-        cst = [0] * training_steps
-        #num_batches = num_instances // batch_size
-        for i in xrange(training_steps):
-            """
-            for j in xrange(num_batches):
-                #lidx = j*batch_size
-                #uidx = min(num_instances, lidx+batch_size)
-                #err = train(X[lidx:uidx], Y[lidx:uidx])
-                err = train(X[j:(j+1)], Y[j:(j+1)])
-            """
-            err = train(X, Y)
-            cst[i] = sum(err)
-            print "%d\t%f" % (i, cst[i])
+    cst = [0] * training_steps
+    X = np.array(X)
+    #num_batches = num_instances // batch_size
+    for i in xrange(training_steps):
+      """
+      for j in xrange(num_batches):
+        #lidx = j*batch_size
+        #uidx = min(num_instances, lidx+batch_size)
+        #err = train(X[lidx:uidx], Y[lidx:uidx])
+        err = train(X[j:(j+1)], Y[j:(j+1)])
+      """
+      err = train(X, Y)
+      cst[i] = sum(err)
+      print "%d\t%f" % (i, cst[i])
 
-        plt.plot(cst)
-        plt.show()
+    plt.plot(cst)
+    #plt.show()
+    plt.savefig('bgd.png')
 
-        self.w = w.get_value()
-        self.b = b.get_value()
+    self.w = w.get_value()
+    self.b = b.get_value()
 
-    def predict_log_proba(self, x):
-        return -log(1 + exp(-(np.dot(x, self.w) + self.b)))
+  def predict_log_proba(self, x, eps=1e-6):
+    #return -log(1 + exp(-(np.dot(x, self.w) + self.b)))
+    p = (1 + tanh(-(np.dot(x, self.w) + self.b))) / 2
+    p = max(p, eps)
+    return log(p)

synth.py
@@ -13,60 +13,60 @@ import numpy as np
 import uuid
 
 def random_song_name():
-    return str(uuid.uuid4())
+  return str(uuid.uuid4())
 
 def generate_synthetic_playsets(
-    num_playsets=100, 
-    num_songs_per_playset=25, 
-    num_songs=100,
-    num_features=50):
-    """
-    Generate random audio feature sets for songs
-    (sample from uniform distribution)
+  num_playsets=100, 
+  num_songs_per_playset=25, 
+  num_songs=100,
+  num_features=50):
+  """
+  Generate random audio feature sets for songs
+  (sample from uniform distribution)
 
-    Generate random playsets by choosing seed songs,
-    then iteratively adding songs with probability
-    proportional to exp(-(L2 distance)) from the working
-    center of the playset
+  Generate random playsets by choosing seed songs,
+  then iteratively adding songs with probability
+  proportional to exp(-(L2 distance)) from the working
+  center of the playset
 
-    :param int num_playsets: Number of playsets
-    :param int num_songs_per_playset: Playset length
-    :param int num_songs: Number of unique songs in data set
-    :param int num_features: Number of audio features
-    :rtype tuple(list[set(str)],dict[str]=AudioFeatureSet)
-    :return A list of playsets containing song IDs, and a dictionary
-    that maps song IDs to AudioFeatureSet objects
-    """
+  :param int num_playsets: Number of playsets
+  :param int num_songs_per_playset: Playset length
+  :param int num_songs: Number of unique songs in data set
+  :param int num_features: Number of audio features
+  :rtype tuple(list[set(str)],dict[str]=AudioFeatureSet)
+  :return A list of playsets containing song IDs, and a dictionary
+  that maps song IDs to AudioFeatureSet objects
+  """
 
-    assert num_songs_per_playset < num_songs, "Playset larger than set of songs!"
+  assert num_songs_per_playset < num_songs, "Playset larger than set of songs!"
 
-    # generate random audio feature sets for songs
-    def random_afs():
-        return np.random.random((num_features,))
+  # generate random audio feature sets for songs
+  def random_afs():
+    return np.random.random((num_features,))
 
-    afshash = {random_song_name():random_afs() for _ in xrange(num_songs)}
+  afshash = {random_song_name():random_afs() for _ in xrange(num_songs)}
 
-    # generate random playsets
-    def random_playset(n):
-        songs = afshash.keys()
-        def sample_from_songs(p=None):
-            r = np.random.choice(range(len(songs)), replace=False, p=p)
-            return (songs[:r]+songs[(r+1):], songs[r])
-        songs, seed = sample_from_songs()
-        ps = [seed]
-        def compute_sample_probabilities(ps):
-            ctr = sum(afshash[x] for x in ps)/len(ps)
-            p = [exp(-np.linalg.norm(ctr-afshash[x])) for x in songs]
-            z = sum(p)
-            return [x/z for x in p]
-        for _ in xrange(n):
-            p = compute_sample_probabilities(ps)
-            songs, new = sample_from_songs(p)
-            ps.append(new)
-        return ps
+  # generate random playsets
+  def random_playset(n):
+    songs = afshash.keys()
+    def sample_from_songs(p=None):
+      r = np.random.choice(range(len(songs)), replace=False, p=p)
+      return (songs[:r]+songs[(r+1):], songs[r])
+    songs, seed = sample_from_songs()
+    ps = [seed]
+    def compute_sample_probabilities(ps):
+      ctr = sum(afshash[x] for x in ps)/len(ps)
+      p = [exp(-np.linalg.norm(ctr-afshash[x])) for x in songs]
+      z = sum(p)
+      return [x/z for x in p]
+    for _ in xrange(n):
+      p = compute_sample_probabilities(ps)
+      songs, new = sample_from_songs(p)
+      ps.append(new)
+    return ps
 
-    playsets = [random_playset(num_songs_per_playset) for _ in xrange(num_playsets)]
+  playsets = [random_playset(num_songs_per_playset) for _ in xrange(num_playsets)]
 
-    afshash = {k:AudioFeatureSet(v) for k, v in afshash.iteritems()}
-    
-    return (playsets, afshash)
+  afshash = {k:AudioFeatureSet(v) for k, v in afshash.iteritems()}
+  
+  return (playsets, afshash)